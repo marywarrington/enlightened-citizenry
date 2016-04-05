35f19ec95b63cd437192f3416bf81cd58b2eac75
Mollie Bootsma and Mary Warrington <mollie.bootsma@gmail.com>
2016-04-05 14:47:32 -0700

    fix completely broken site

--- app/routes/bills-results.js
+++ app/routes/bills-results.js
@@ -1,3 +1,4 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
@@ -8,3 +9,22 @@ export default Ember.Route.extend({
     });
   }
 });
+
+
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   model(params) {
+//     var url = 'http://congress.api.sunlightfoundation.com/'
+//     return Ember.RSVP.hash({
+//       bills: Ember.$.getJSON(url + 'bills?apikey=c1ea92cc48034bd9bb5d333898f200f3'),
+//       sponsor: Ember.$.getJSON(url + 'legislators?bioguide_id=' + params.id + '&apikey=c1ea92cc48034bd9bb5d333898f200f3')
+//     });
+//     return responseJSON.results;
+//   },
+//   actions: {
+//     sponsorIdLookup(params) {
+//       this.transitionTo('bills-results', params.id);
+//     }
+//   }
+// });
